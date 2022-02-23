bd18992a14c4ff466dde464b937f04df4586652b
Leo <aammlj2002@gmail.com>
2022-02-23 16:03:21 +0630

    react component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-    return <h1>hello react</h1>;
+import React from "react";
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>hello react</h1>
+                <ul>
+                    <Item />
+                    <Item />
+                </ul>
+            </div>
+        );
+    }
 }
-
+class Item extends React.Component {
+    render() {
+        return <li>item</li>;
+    }
+}
 export default App;
