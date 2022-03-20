540b1d94b21a6d92825cb6a24c51ab7439ddad5d
Leo <aammlj2002@gmail.com>
2022-03-20 17:30:08 +0630

    routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import React from "react";
-const MyContext = React.createContext("hello react");
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+const users = [
+    { id: 1, name: "Alice", gender: "f" },
+    { id: 2, name: "Bob", gender: "m" },
+    { id: 3, name: "Tom", gender: "m" },
+    { id: 4, name: "Mary", gender: "f" },
+];
+
 const App = (props) => {
-    return <Header />;
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/male">Male</Link>
+                    </li>
+                    <li>
+                        <Link to="/female">Female</Link>
+                    </li>
+                </ul>
+                <div style={{ background: "cyan", padding: 20 }}>
+                    <Routes>
+                        <Route path="/male" element={<Male />} />
+                        <Route path="/female" element={<Female />} />
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 };
-const Header = (props) => {
-    return <Title />;
+const Male = (props) => {
+    return (
+        <ul>
+            {users
+                .filter((u) => u.gender === "m")
+                .map((u) => (
+                    <li key={u.id}>{u.name}</li>
+                ))}
+        </ul>
+    );
 };
-const Title = (props) => {
-    const name = React.useContext(MyContext);
-    return <h1>{name}</h1>;
+const Female = (props) => {
+    return (
+        <ul>
+            {users
+                .filter((u) => u.gender === "f")
+                .map((u) => (
+                    <li key={u.id}>{u.name}</li>
+                ))}
+        </ul>
+    );
 };
 export default App;
