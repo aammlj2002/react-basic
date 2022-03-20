56c9a0337ab4607aa8107bc092b3a5c3be754ded
Leo <aammlj2002@gmail.com>
2022-03-20 15:42:36 +0630

    state in function component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,25 @@
 import React from "react";
 import Header from "./Header";
-class App extends React.Component {
-    state = {
-        items: [
-            { id: 1, name: "Apple", price: 0.99 },
-            { id: 2, name: "mango", price: 0.89 },
-        ],
+const App = () => {
+    let [items, setItems] = React.useState([
+        { id: 1, name: "Apple", price: 0.88 },
+        { id: 2, name: "orange", price: 0.99 },
+    ]);
+    let add = () => {
+        setItems([...items, { id: 3, name: "Banana", price: 0.75 }]);
     };
-    add = (name, price) => {
-        let id = this.state.items.length + 1;
-        this.setState({
-            items: [...this.state.items, { id, name, price }],
-        });
-    };
-    render() {
-        return (
-            <div>
-                <InputForm add={this.add} />
-                <ul>
-                    {this.state.items.map((i) => {
-                        return (
-                            <Item key={i.id} name={i.name} price={i.price} />
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
-}
-class InputForm extends React.Component {
-    nameRef = React.createRef();
-    priceRef = React.createRef();
-    add = () => {
-        let name = this.nameRef.current.value;
-        let price = this.priceRef.current.value;
-        this.props.add(name, price);
-    };
-    render() {
-        return (
-            <div>
-                <Header name="react"></Header>
-                <input type="text" ref={this.nameRef}></input>
-                <br />
-                <input type="text" ref={this.priceRef}></input>
-                <br />
-                <button onClick={this.add}>add item</button>
-            </div>
-        );
-    }
-}
+    return (
+        <div>
+            <Header name="react"></Header>
+            <ul>
+                {items.map((i) => (
+                    <Item name={i.name} price={i.price}></Item>
+                ))}
+            </ul>
+            <button onClick={add}>add</button>
+        </div>
+    );
+};
 
 const Item = ({ name, price }) => (
     <li>
