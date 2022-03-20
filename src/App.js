66a0621ad0137da0b87f5f8cfade349119975c1f
Leo <aammlj2002@gmail.com>
2022-03-20 14:56:08 +0630

    props method

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React from "react";
 class App extends React.Component {
-    nameRef = React.createRef();
-    priceRef = React.createRef();
-    add = () => {
-        let id = this.state.items.length + 1;
-        let name = this.nameRef.current.value;
-        let price = this.priceRef.current.value;
-        this.setState({
-            items: [...this.state.items, { id, name, price }],
-        });
-    };
     state = {
         items: [
             { id: 1, name: "Apple", price: 0.99 },
             { id: 2, name: "mango", price: 0.89 },
         ],
     };
+    add = (name, price) => {
+        let id = this.state.items.length + 1;
+        this.setState({
+            items: [...this.state.items, { id, name, price }],
+        });
+    };
     render() {
+        return (
+            <div>
+                <InputForm add={this.add}/>
+                <ul>
+                    {this.state.items.map((i) => {
+                        return (
+                            <Item key={i.id} name={i.name} price={i.price} />
+                        );
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
+class InputForm extends React.Component{
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+    add = ()=>{
+        let name = this.nameRef.current.value;
+        let price = this.priceRef.current.value;
+        this.props.add(name, price);
+    }
+    render(){
         return (
             <div>
                 <h1>hello react</h1>
@@ -25,13 +44,6 @@ class App extends React.Component {
                 <input type="text" ref={this.priceRef}></input>
                 <br />
                 <button onClick={this.add}>add item</button>
-                <ul>
-                    {this.state.items.map((i) => {
-                        return (
-                            <Item key={i.id} name={i.name} price={i.price} />
-                        );
-                    })}
-                </ul>
             </div>
         );
     }
