59177495b0080c421207ebd41ffd573a5358fe88
Leo <aammlj2002@gmail.com>
2022-03-20 15:58:10 +0630

    context

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from "react";
-import Header from "./Header";
-const App = () => {
-    let [items, setItems] = React.useState([
-        { id: 1, name: "Apple", price: 0.88 },
-        { id: 2, name: "orange", price: 0.99 },
-    ]);
-    let add = () => {
-        setItems([...items, { id: 3, name: "Banana", price: 0.75 }]);
-    };
-    return (
-        <div>
-            <Header name="react"></Header>
-            <ul>
-                {items.map((i) => (
-                    <Item name={i.name} price={i.price}></Item>
-                ))}
-            </ul>
-            <button onClick={add}>add</button>
-        </div>
-    );
+const MyContext = React.createContext("hello react");
+const App = (props) => {
+    return <Header />;
 };
-
-const Item = ({ name, price }) => (
-    <li>
-        {name} is ${price}
-    </li>
-);
-
+const Header = (props) => {
+    return <Title />;
+};
+const Title = (props) => {
+    const name = React.useContext(MyContext);
+    return <h1>{name}</h1>;
+};
 export default App;
