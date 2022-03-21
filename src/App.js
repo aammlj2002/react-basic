871daf48e904d188bce81c10907791c8422b8192
Leo <aammlj2002@gmail.com>
2022-03-21 14:35:06 +0630

    fetch data form api

--- src/App.js
+++ src/App.js
@@ -1,65 +1,22 @@
-import React from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-    useParams,
-} from "react-router-dom";
-const users = [
-    { id: 1, name: "Alice", gender: "f" },
-    { id: 2, name: "Bob", gender: "m" },
-    { id: 3, name: "Tom", gender: "m" },
-    { id: 4, name: "Mary", gender: "f" },
-];
+import React, { useEffect, useState } from "react";
+const App = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch("https://reqres.in/api/users")
+            .then((res) => res.json())
+            .then((json) => {
+                setUsers(json.data);
+            });
+    }, []);
 
-const App = (props) => {
     return (
-        <Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/male">Male</Link>
-                    </li>
-                    <li>
-                        <Link to="/female">Female</Link>
-                    </li>
-                </ul>
-                <div style={{ background: "cyan", padding: 20 }}>
-                    <Routes>
-                        <Route path="/user/:name" element={<User />} />
-                        <Route path="/male" element={<Male />} />
-                        <Route path="/female" element={<Female />} />
-                    </Routes>
-                </div>
-            </div>
-        </Router>
-    );
-};
-const User = (props) => {
-    const { name } = useParams();
-    return <h1>Profile-{name}</h1>;
-};
-const Male = (props) => {
-    return (
-        <ul>
-            {users
-                .filter((u) => u.gender === "m")
-                .map((u) => (
-                    <li key={u.id}>{u.name}</li>
+        <div>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.first_name}</li>
                 ))}
-        </ul>
-    );
-};
-const Female = (props) => {
-    return (
-        <ul>
-            {users
-                .filter((u) => u.gender === "f")
-                .map((u) => (
-                    <li key={u.id}>{u.name}</li>
-                ))}
-        </ul>
+            </ul>
+        </div>
     );
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore((state = [], action) => {
+    if (action.type === "ADD") return [...state, action.item];
+    return state;
+});
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
